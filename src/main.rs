41bf9bce7574e83a71117dd064666f0715d782e1
Siddharth Doshi <sidoshi510@gmail.com>
2025-07-10 16:56:54 +0200

    allow cors

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod html2pdf;
 use std::sync::Arc;
 
 use anyhow::Result;
+use axum::http::Method;
 use axum::{
     Router,
     routing::{get, post},
 };
+use tower_http::cors::{Any, CorsLayer};
 
 use browser_pool::BrowserPool;
 use html2pdf::html2pdf;
@@ -19,10 +21,17 @@ async fn main() -> Result<()> {
 
     let browser_pool = Arc::new(BrowserPool::new().await?);
 
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers(Any)
+        .allow_credentials(false);
+
     let app = Router::new()
         .route("/healthz", get(healthz))
         .route("/html2pdf", post(html2pdf))
-        .with_state(browser_pool);
+        .with_state(browser_pool)
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
